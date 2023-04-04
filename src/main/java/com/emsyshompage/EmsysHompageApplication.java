27bb08e72980fc71feb8428cde702feecfa06897
강명석 <k4858678@gmail.com>
2023-04-05 00:41:32 +0900

    first test

---
+++ src/main/java/com/emsyshompage/EmsysHompageApplication.java
@@ -0,0 +1,13 @@
+package com.emsyshompage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsysHompageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsysHompageApplication.class, args);
+    }
+
+}
