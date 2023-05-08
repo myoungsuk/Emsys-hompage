f19f8c7e012c664b88ffb6d03e9e762130908964
강명석 <k4858678@gmail.com>
2023-05-08 23:57:30 +0900

    Article ArticleComment 데이터 설정 테스트코드 실행

--- src/main/java/com/emsys/homepage/domain/Article.java
+++ src/main/java/com/emsys/homepage/domain/Article.java
@@ -1,11 +1,35 @@
 package com.emsys.homepage.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+
+})
+
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String hashtag;
+
+
+
+    @Setter private String title; //제목
+    @Setter private String content; //내용
+
+    @Setter private String hashtag;
 
     private LocalDateTime createdAt;
     private String createdBy;
