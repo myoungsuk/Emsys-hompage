2bf8b3436bc279e0885fa14103b646fc2f0dd5e4
강명석 <k4858678@gmail.com>
2023-05-09 21:17:58 +0900

    데이터베이스 접근로직 테스트 정의

--- src/main/java/com/emsys/homepage/repository/ArticleCommentRepository.java
+++ src/main/java/com/emsys/homepage/repository/ArticleCommentRepository.java
@@ -3,6 +3,7 @@ package com.emsys.homepage.repository;
 import com.emsys.homepage.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 
--- src/test/java/com/emsys/homepage/repository/JpaRepositoryTest.java
+++ src/test/java/com/emsys/homepage/repository/JpaRepositoryTest.java
@@ -1,11 +1,20 @@
 package com.emsys.homepage.repository;
 
 import com.emsys.homepage.config.JpaConfig;
+import com.emsys.homepage.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@ActiveProfiles("testdb")
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
@@ -23,10 +32,26 @@ class JpaRepositoryTest {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
     }
-    @DisplayName("select 테스트")
+
+    @DisplayName("delete 테스트")
     @Test
-    void givenTestDate_whenSelecting_thenWorksFine(){
-        System.out.println(articleRepository.findAll());
-        System.out.println(articleCommentRepository.findAll());
+    void givenTestDate_whenDeleting_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow(IllegalArgumentException::new);
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deleteCommentSize = article.getArticleComments().size();
+
+
+
+
+        //when
+        articleRepository.delete(article);
+
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deleteCommentSize);
     }
+
 }
