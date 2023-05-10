7bf08e36552bbe87456a2d4b17afd6fc5389e927
강명석 <k4858678@gmail.com>
2023-05-10 23:18:06 +0900

    API구현 검색기능 레포지터리 추가

--- src/main/java/com/emsys/homepage/repository/ArticleCommentRepository.java
+++ src/main/java/com/emsys/homepage/repository/ArticleCommentRepository.java
@@ -1,11 +1,34 @@
 package com.emsys.homepage.repository;
 
+import com.emsys.homepage.domain.Article;
 import com.emsys.homepage.domain.ArticleComment;
+import com.emsys.homepage.domain.QArticle;
+import com.emsys.homepage.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+    QuerydslPredicateExecutor<ArticleComment>,
+    QuerydslBinderCustomizer<QArticleComment>{
 
 
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including( root.content, root.createdAt, root.createdBy);
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);      // like 's{v}'
+//        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  // like '%s{v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+//        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
--- src/main/java/com/emsys/homepage/repository/ArticleRepository.java
+++ src/main/java/com/emsys/homepage/repository/ArticleRepository.java
@@ -1,11 +1,35 @@
 package com.emsys.homepage.repository;
 
 import com.emsys.homepage.domain.Article;
+import com.emsys.homepage.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long>
-{
+import static antlr.build.ANTLR.root;
 
+@RepositoryRestResource
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+       bindings.excludeUnlistedProperties(true);
+       bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);      // like 's{v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  // like '%s{v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
+
+
