8686e2ab14f00da00a4cdd267fa9f7dd4e2f99e8
강명석 <k4858678@gmail.com>
2023-05-19 19:22:52 +0900

    게시판 페이지 기능 구현

--- src/main/java/com/emsys/homepage/controller/ArticleController.java
+++ src/main/java/com/emsys/homepage/controller/ArticleController.java
@@ -3,7 +3,9 @@ package com.emsys.homepage.controller;
 
 import com.emsys.homepage.dto.response.ArticleWithCommentsResponse;
 import com.emsys.homepage.service.ArticleService;
+import com.emsys.homepage.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -27,6 +29,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -35,7 +38,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
